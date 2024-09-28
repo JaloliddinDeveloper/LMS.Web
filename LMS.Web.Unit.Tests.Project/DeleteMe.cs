f5f838ad1447d14758bcd54a5c72c213cfbe9d74
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-09-29 01:29:13 +0500

    INFRA:LMS Web Unit Tests Project Initialize

---
+++ LMS.Web.Unit.Tests.Project/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+namespace LMS.Web.Unit.Tests.Project
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue()=>
+            Assert.True(true);
+    }
+}
